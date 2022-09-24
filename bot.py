7e79a60861169987af636268e776bab34850ac71
gabribn99 <gabribn99@gmail.com>
2022-09-24 18:46:18 +0100

    Start commit

---
+++ bot.py
@@ -0,0 +1,15 @@
+import pyautogui
+import webbrowser
+from time import sleep
+
+phoneNumber = 'PHONE'
+textToSend = 'FILENAME.txt'
+
+webbrowser.open('https://api.whatsapp.com/send/?phone=34' + phoneNumber)
+
+sleep(2)
+
+with open(textToSend, 'r') as file:
+    for line in file:
+        pyautogui.typewrite(line)
+        pyautogui.press("enter")
