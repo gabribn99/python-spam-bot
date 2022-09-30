92b18187f808740c1576324c58df20b8639e34cc
gabribn99 <gabribn99@gmail.com>
2022-09-30 16:09:17 +0100

    Mejora del bot

--- bot.py
+++ bot.py
@@ -3,14 +3,31 @@ import webbrowser
 from time import sleep
 
 phoneNumber = input('Indique el número de teléfono: ')
+sendType = int(
+    input('Indique el tipo de envío: \n0. Reiterado\n1. Por linea\n>'))
 
 textToSend = 'text.txt'
 
-webbrowser.open('https://api.whatsapp.com/send/?phone=34' +
-                phoneNumber.replace(' ', ''))
-sleep(2)
 
-with open(textToSend, 'r') as file:
-    for line in file:
-        pyautogui.typewrite(line)
+def openWhatsapp():
+    webbrowser.open('https://api.whatsapp.com/send/?phone=34' +
+                    phoneNumber.replace(' ', ''))
+    sleep(2)
+
+
+if sendType == 0:
+    times = int(
+        input('Indique la cantidad de veces que desea enviar el mensaje: '))
+    text = input('Indique el texto que desea enviar: ')
+    openWhatsapp()
+    for i in range(times):
+        pyautogui.typewrite(text)
         pyautogui.press("enter")
+
+if sendType == 1:
+    openWhatsapp()
+    with open(textToSend, 'r') as file:
+        for line in file:
+            for word in line.split(' '):
+                pyautogui.typewrite(word)
+                pyautogui.press("enter")
